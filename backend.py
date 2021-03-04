6a0903729b397a1b901fed52bb2d18bd9eef2b86
Osama Ibraheem <samatube10@gmail.com>
2021-03-04 09:00:48 -0600

    Rendering POS based off language

--- backend.py
+++ backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 from flaskext.mysql import MySQL
 from datetime import datetime
 import logic
@@ -17,16 +17,17 @@ cursor = conn.cursor()
 def hello():
     cursor.execute("SELECT Lang_Desc FROM Lang_Ref;")
     conn.commit()
-    language_list = logic.tuple2list(cursor.fetchall()) # Uses function from logic file to convert tuple to list 
-    return render_template('index.html', language_list = language_list)
+    language_list = logic.tuple2list(cursor.fetchall()) # Uses function from logic file to convert tuple to list
+    part_of_speech_list = logic.SQLQuery("SELECT Part_Desc FROM Speech_Parts WHERE Lang_ID = 1;")
+    return render_template('index.html', language_list = language_list,part_of_speech_list=part_of_speech_list)
 
-@app.route('/test', methods =["GET", "POST"])
-def test():
+@app.route('/Query', methods =["GET", "POST"])
+def Query():
     language_list = logic.SQLQuery("SELECT Lang_Desc FROM Lang_Ref;")
     language_selected = request.form.get('language')
     Lang_ID = logic.SQLQuery(f"SELECT Lang_ID FROM Lang_Ref WHERE Lang_Desc = '{language_selected}';")
     part_of_speech_list = logic.SQLQuery(f"SELECT Part_Desc FROM Speech_Parts WHERE Lang_ID = '{Lang_ID[0]}';")
-    return render_template('index.html', language_list = language_list, part_of_speech_list = part_of_speech_list)
+    return render_template('partofspeech.html', part_of_speech_list=part_of_speech_list ) 
 
 
 if __name__ == '__main__':
