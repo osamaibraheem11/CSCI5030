0dc3979c94d0024e2f815786d6917721a0387e14
Pankil Shah <pnklshh06@hotmail.com>
2021-03-17 21:12:04 -0500

    Changes in load corpus code as suggested in code review

--- parser.py
+++ parser.py
@@ -2,44 +2,62 @@
 # Created on: 03/13/2021
 # Description: Parse a corpus and dump the sentences into the database 
 
-from nltk.corpus import brown
+# NLTK is the library used for working with human language data for applying in statistical natural language processing (NLP)
 import nltk.data
 import logic
 import datetime
+import sys
 from os import listdir
 from os.path import isfile, join
 
-#print(brown.tagged_sents()[0:2])
+def GetLanguageId(language):
+    language_id_list = logic.SQLQuery("select Lang_ID from lang_ref where Lang_Desc = '" + language + "'")
 
-#dirPath = 'E:\\SLU\\Sem1\\Principles of SD\\Project Material\\brown'
+    # Condition for language not present in database
+    if len(language_id_list) == 0:
+        return -1
+    else:
+        return language_id_list[0]
+
+# Example path: 'E:\\SLU\\Sem1\\Principles of SD\\Project Material\\brown'
 path = input("Enter path of the corpus directory: ")
 language = input("Enter language: ")
-#language = "english"
+
+language_id = str(GetLanguageId(language))
+
+if language_id == "-1":
+    sys.exit("Language not found in the database.")
 
 # list of all file names in the corpus directory
-fileNameList = [f for f in listdir(path) if isfile(join(path, f))]
-languageId = str(logic.SQLQuery("select Lang_ID from lang_ref where Lang_Desc = '" + language + "'")[0])
+file_name_list = []
+for file_name in listdir(path):
+    if isfile(join(path, file_name)):
+        file_name_list.append(file_name)
 
-for fileName in fileNameList:
+line_id = 1
+document_id = 1
+values = ""
+for file_name in file_name_list:
 
     # ignore the files CONTENTS and README
-    if (fileName != "CONTENTS") and (fileName != "README"):
-        filePath = path + '\\' + fileName
-        with open(filePath, 'r') as file:
+    if (file_name != "CONTENTS") and (file_name != "README"):
+        file_path = path + '\\' + file_name
+        with open(file_path, 'r') as file:
             text = file.read().replace('\n', '')
 
         sent_detector = nltk.data.load('tokenizers/punkt/' + language.lower() + '.pickle')
-        sentenceList = sent_detector.tokenize(text.strip())
+        sentence_list = sent_detector.tokenize(text.strip())
 
-        values = ""
-        # form the values string required in insert query
-        for sentence in sentenceList:
-            values += "(" + languageId + ", 1, " + "'" + fileName + "', " + "\"" + sentence + "\", " + "'" + str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + "'),"
+        for sentence in sentence_list:
+            # form the values string required in insert query
+            values += "(" + language_id + ", " + str(document_id) + ", " + "'" + file_name + "', " + "\"" + sentence + "\", " + "'" + str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + "'),"
+            line_id += 1
 
-        # remove last comma from values string    
-        values = values[:-1]
+        document_id += 1
 
-        corpusTable = language.lower() + "_corpus"
-        query = "insert into " + corpusTable + " (Lang_ID, Doc_ID, Doc_Name, Line_Text, Last_Update) values " + values
-        logic.SQLInsertQuery(query)
+# remove last comma from values string    
+values = values[:-1]
 
+corpus_table = language.lower() + "_corpus"
+query = "insert into " + corpus_table + " (Lang_ID, Doc_ID, Doc_Name, Line_Text, Last_Update) values " + values
+logic.SQLInsertQuery(query)
