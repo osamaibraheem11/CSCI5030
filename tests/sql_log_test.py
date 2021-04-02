66b69a28ce6cee4c95b4d64775b375209c264f5d
Osama Ibraheem <samatube10@gmail.com>
2021-04-02 08:55:52 -0500

    added testing folder

--- tests/sql_log_test.py
+++ tests/sql_log_test.py
@@ -1,10 +1,11 @@
-import sys
+import sys,os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import csv
 import logic
 
 
 def write_2_log_test():
-   read_csv = csv.reader(open("sql_log_test.csv",encoding = "utf8" ))
+   read_csv = csv.reader(open("tests/sql_log_test.csv",encoding = "utf8" ))
    for row in read_csv: 
       test_statement, answer_statement, test_status, answer_status, test_purpose, answer_purpose = row
       logic.SQL_log(test_statement,test_status,test_purpose)
