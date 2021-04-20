b6d078033ea5789650e8908980ac90a40808df9b
Osama Ibraheem <samatube10@gmail.com>
2021-04-20 10:39:57 -0500

    added logging config

--- word2vec.py
+++ word2vec.py
@@ -3,3 +3,5 @@ from gensim.test.utils import datapath
 from gensim.parsing.preprocessing import remove_stopwords
 from gensim import utils
 import logging
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
