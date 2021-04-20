bd95d45b0dcc7194baedb2ce40c583d0b0cfa813
Osama Ibraheem <samatube10@gmail.com>
2021-04-20 10:43:24 -0500

    added corpus class

--- word2vec.py
+++ word2vec.py
@@ -5,3 +5,11 @@ from gensim import utils
 import logging
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+class MyCorpus(object):
+    def __init__(self,path):
+        self.path = path
+    def __iter__(self):
+        corpus_path = datapath(self.path)
+        for line in open(corpus_path):
+            yield utils.simple_preprocess(line)
